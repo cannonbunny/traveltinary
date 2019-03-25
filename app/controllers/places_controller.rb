da5248747c67e129214831512e5543dea07770ab
C Y Law <gen.2.gentoo@gmail.com>
2019-03-25 01:06:34 -0400

    Add show/edit/update/destroy in controller and the corresponding route files

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,12 @@
 class PlacesController < ApplicationController
 
+before_action :find_place, only: [:show, :edit, :update, :destroy]
+
   def index
+    @place = Place.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -9,6 +15,27 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new(place_params)
+    if @place.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @place.update(place_params)
+      redirect_to place_path(@place)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @place.destroy
+    redirect_to root_path
   end
 
 private
@@ -16,5 +43,9 @@ private
   def place_params
     params.require(:place).permit(:continent, :country, :city, :description)
   end
+
+  def find_place
+   @place = Place.find(params[:id])
+ end
 
 end
