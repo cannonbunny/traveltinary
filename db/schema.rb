fac7bd33d31cf04a9df0ace30294bedc1ad8469d
C Y Law <gen.2.gentoo@gmail.com>
2019-03-26 02:34:01 -0400

    Add review model/schema to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_035037) do
+ActiveRecord::Schema.define(version: 2019_03_26_063212) do
 
   create_table "continents", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2019_03_26_035037) do
     t.integer "user_id"
     t.integer "continent_id"
     t.string "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "vote"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "place_id"
   end
 
   create_table "users", force: :cascade do |t|
