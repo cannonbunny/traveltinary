f6385d8a4c7fc76a0c1911a7f7ef4f0053785b08
C Y Law <chevy.cy.law@gmail.com>
2019-04-06 22:13:53 -0400

    Add carrierwave and aws files to enable image storage on aws instead of pg

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],        # required unless using use_iam_profile
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],        # required unless using use_iam_profile
+    region:                'us-west-2',                  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = ENV['AWS_BUCKET']                                      # required
+end
